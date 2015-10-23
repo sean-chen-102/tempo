5e969c66b11a86925a7ddddd9989849644a762cf
Dalton Boll <daltonboll@gmail.com>
2015-10-23 03:12:29 -0700

    Add multiple interests functionality to get_activities API query

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -23,25 +23,37 @@ class Activity < ActiveRecord::Base
 	validates :completion_time, numericality: { greater_than: 0,
 											    less_than_or_equal_to: 60 }
 
-  # Returns a JSON list of all activities that have interest_name as their Interest.name.
-  # If interest_name is nil, it returns a JSON list of all Activities in the database.
-  def self.get_activities(interest_name, time)
-    if interest_name.nil?
+  # Returns a JSON list of all activities that match the Interests provided in interests_list, with
+  # completion_time less than or equal to time.
+  # If interests_list is nil, it returns a JSON list of all Activities in the database.
+  def self.get_activities(interests_list, time)
+    activities_list = []
+
+    if interests_list.nil?
       activities = Activity.all
     else
-      activities = Activity.joins(:interests).where(interests: {name: interest_name})
+      activities = Activity.joins(:interests).distinct.where(interests: {name: interests_list})
     end
 
     if not time.nil?
       activities = activities.where("completion_time <= ?", time)
     end
 
-    activities.each do |activity|
-      activity = activity.to_json
+    activities = activities.as_json
+    return activities
+  end
+
+  # Returns a string of the form: "name = <name1> or name = <name2>"...
+  # Takes in a list of Interest names
+  def convert_names_to_sql_arg_string(interest_names)
+    arg_string = ""
+
+    interest_names.each do |name|
+      arg_string = arg_string + "name = #{name} or "
     end
 
-    activities = activities.to_json
-    return activities
+    arg_string.chomp(" or ")
+    return arg_string
   end
 
 end

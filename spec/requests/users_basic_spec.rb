666359d5ec934b1211716289143cfd186904fa4b
Dalton Boll <daltonboll@gmail.com>
2015-11-06 04:09:29 -0800

    Finish getting a single User tests

--- spec/requests/users_basic_spec.rb
+++ spec/requests/users_basic_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "test basic users functionality", :type => :request do
+RSpec.describe "test basic users functionality - ", :type => :request do
 
 	### USER CREATION ###
 
@@ -52,6 +52,49 @@ RSpec.describe "test basic users functionality", :type => :request do
   end
 
 	### GET USER ###
+
+  it "getting a single user" do
+    # Create an account
+    params = { "user": { "name": "Bob", "email": "bob@mail.com", "username": "bob", "password": "password", "password_confirmation": "password" }}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(1) # we should have a success
+
+    user = data["user"]
+    id = user["id"]
+
+    # Get the user
+    get "/api/users/#{id}", {}, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(1) # we should have a success
+
+    # Verify that the user data is consistent
+    user = data["user"]
+    expect(id).to eq(user["id"])
+    expect(user["name"]).to eq("Bob")
+    expect(user["email"]).to eq("bob@mail.com")
+    expect(user["username"]).to eq("bob")
+
+    # Try and get a user that doesn't exist
+    get "/api/users/hah", {}, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a success
+
+    # Try and get a user that doesn't exist
+    get "/api/users/-1", {}, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a success
+
+    # Try and get a user that doesn't exist
+    get "/api/users/0", {}, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a success
+  end
 
 	### GET ALL USERS ###
 

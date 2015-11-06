714ca838583303e9065d2fd61858b58ec0147d51
Dalton Boll <daltonboll@gmail.com>
2015-11-05 23:49:16 -0800

    Update user account creation test

--- spec/requests/account_creation_spec.rb
+++ spec/requests/account_creation_spec.rb
@@ -8,12 +8,24 @@ RSpec.describe "account creation", :type => :request do
   it "returns a valid user after account creation" do
     params = { "user": { "name": "Bob", "email": "bob@mail.com", "username": "bob", "password": "password", "password_confirmation": "password" }}
     post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
-    #=> params contains { "controller" => "user", "action" => "reset_password", "token" => 0 }
 
-    data = JSON.parse(response.body)
-    puts "WE ARE HERE!"
-    puts "#{data}"
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(1) # we should have a success
 
-    expect(data["status"]).to equal(1)
+    user = data["user"]
+    id = user["id"]
+    name = user["name"]
+    username = user["username"]
+    created_at = user["created_at"]
+    updated_at = user["updated_at"]
+    interests = user["interests"]
+    custom_activities = user["custom_activities"]
+    token = data["token"]
+
+    expect(name).to eq("Bob")
+    expect(username).to eq("bob")
+    expect(interests).to eq([])
+    expect(custom_activities).to eq([])
   end
 end

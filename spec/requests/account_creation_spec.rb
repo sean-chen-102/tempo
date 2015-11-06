6dbe74bbb46ec6582a2eb5ba6d706e43ab091cbe
Dalton Boll <daltonboll@gmail.com>
2015-11-06 01:23:16 -0800

    Finish User account creation testing

--- spec/requests/account_creation_spec.rb
+++ spec/requests/account_creation_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 # POST /api/users/
 # Testing via curl: curl -H "Content-Type: application/json" -X POST -d '{"user": {"name": "Jack Daniels", "email": "jack6@mail.com", "username": "jackD6", "password": "password", "password_confirmation": "password"}}' http://localhost:3000/api/users
 RSpec.describe "account creation", :type => :request do
+  # CREATE A NEW USER
   it "returns a valid user after account creation" do
     params = { "user": { "name": "Bob", "email": "bob@mail.com", "username": "bob", "password": "password", "password_confirmation": "password" }}
     post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
@@ -27,5 +28,136 @@ RSpec.describe "account creation", :type => :request do
     expect(username).to eq("bob")
     expect(interests).to eq([])
     expect(custom_activities).to eq([])
+  end
+
+  # TRY CREATING A NEW USER WITH INFO THAT ALREADY EXISTS IN THE DATABASE
+  it "does not allow duplicate account creation" do
+    # Create a unique User
+    params = { "user": { "name": "Bob", "email": "bob@mail.com", "username": "bob", "password": "password", "password_confirmation": "password" }}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data1 = JSON.parse(response.body) # grab the body of the server response
+    status1 = data1["status"]
+    expect(status1).to eq(1) # we should have a success
+    user1 = data1["user"]
+
+    # Create a User with exactly the same fields
+    params = { "user": { "name": "Bob", "email": "bob@mail.com", "username": "bob", "password": "password", "password_confirmation": "password" }}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data2 = JSON.parse(response.body) # grab the body of the server response
+    status2 = data2["status"]
+    expect(status2).to eq(-1) # we should have a failure
+    user2 = data2["user"]
+
+    # Create a User with a duplicate email
+    params = { "user": { "name": "Tracy", "email": "bob@mail.com", "username": "tracy", "password": "password", "password_confirmation": "password" }}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data2 = JSON.parse(response.body) # grab the body of the server response
+    status2 = data2["status"]
+    expect(status2).to eq(-1) # we should have a failure
+    user2 = data2["user"]
+
+    # Create a User with a duplicate username
+    params = { "user": { "name": "Tracy", "email": "tracy@mail.com", "username": "bob", "password": "password", "password_confirmation": "password" }}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data2 = JSON.parse(response.body) # grab the body of the server response
+    status2 = data2["status"]
+    expect(status2).to eq(-1) # we should have a failure
+    user2 = data2["user"]
+
+    # Create a User with a duplicate username and email
+    params = { "user": { "name": "Tracy", "email": "bob@mail.com", "username": "bob", "password": "password", "password_confirmation": "password" }}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data2 = JSON.parse(response.body) # grab the body of the server response
+    status2 = data2["status"]
+    expect(status2).to eq(-1) # we should have a failure
+    user2 = data2["user"]
+
+    # Create a User with a duplicate name - should succeed
+    params = { "user": { "name": "Bob", "email": "shelly@mail.com", "username": "shelly", "password": "password", "password_confirmation": "password" }}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data2 = JSON.parse(response.body) # grab the body of the server response
+    status2 = data2["status"]
+    expect(status2).to eq(1) # we should have a success
+    user2 = data2["user"]
+  end
+
+  # Create a User with the password confirmation included - should succeed
+  it "without the password confirmation" do
+    params = { "user": { "name": "Bob", "email": "bob@mail.com", "username": "bob", "password": "password" }}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(1) # we should have a success
+  end
+
+  # Create a User with mismatching password and password confirmation
+  it "with mismatching password and password_confirmation" do
+    params = { "user": { "name": "Bob", "email": "bob@mail.com", "username": "bob", "password": "password", "password_confirmation": "hi"}}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a failure
+  end
+
+  # Create a User with info that doesn't meet the validation standards
+  it "with bad model validation credentials" do
+    params = { "user": { "name": "", "email": "bob@mail.com", "username": "bob", "password": "password", "password_confirmation": "password"}}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a failure
+
+    params = { "user": { "name": "Bob", "email": "", "username": "bob", "password": "password", "password_confirmation": "password"}}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a failure
+
+    params = { "user": { "name": "Bob", "email": "bob@mail.com", "username": "", "password": "password", "password_confirmation": "password"}}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a failure
+
+    params = { "user": { "name": "Bob", "email": "bob@mail.com", "username": "", "password": "", "password_confirmation": ""}}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a failure
+
+    params = { "user": { "name": "Bob", "email": "mail.com", "username": "bob", "password": "password", "password_confirmation": "password"}}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a failure
+
+    params = { "user": { "name": "Bobasdfsdfasdfasfdsafdasdfasdfsdfsdf", "email": "mail.com", "username": "bob", "password": "password", "password_confirmation": "password"}}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a failure
+
+    params = { "user": { "name": "Bob", "email": "mail.com", "username": "basdfasdfasdfasdfasdfasdfsadfob", "password": "password", "password_confirmation": "password"}}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a failure
+
+    params = { "user": { "name": "Bob", "email": "b@mail.com", "username": "b2_-~r0%@!#ob", "password": "password", "password_confirmation": "password"}}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a failure
+  end
+
+  # SEND MISSING PARAMS
+  it "with missing params attributes" do
+    params = { "user": { "name": "hi"} }
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a failure
   end
 end

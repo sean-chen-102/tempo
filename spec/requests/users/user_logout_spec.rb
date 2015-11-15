04203b7a7e012134f3678fc4c2f04235df786962
Dalton Boll <daltonboll@gmail.com>
2015-11-15 15:58:10 -0800

    Fix failing logout test

--- spec/requests/users/user_logout_spec.rb
+++ spec/requests/users/user_logout_spec.rb
@@ -5,9 +5,26 @@ require 'rails_helper'
 RSpec.describe "user log out - ", :type => :request do
   # Sign out
   it "succeeds with log out process" do
-    delete "/api/logout", {}, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    # Create an account
+    params = { "user": { "name": "Bob", "email": "bob@mail.com", "username": "bob", "password": "password", "password_confirmation": "password" }}
+    post "/api/users", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
     data = JSON.parse(response.body) # grab the body of the server response
     status = data["status"]
     expect(status).to eq(1) # we should have a success
+    user = data["user"]
+    token = user["token"]
+
+    params = { "token": token }
+    delete "/api/logout", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(1) # we should have a success
+  end
+
+  it "should fail with invalid token" do
+    delete "/api/logout", {}, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    data = JSON.parse(response.body) # grab the body of the server response
+    status = data["status"]
+    expect(status).to eq(-1) # we should have a failure
   end
 end

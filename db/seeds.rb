29f7c880bc8c047be1a50c1e0886beb85abe5599
Dalton Boll <daltonboll@gmail.com>
2015-11-15 22:05:05 -0800

    Populate database with history-based activities

--- db/seeds.rb
+++ db/seeds.rb
@@ -334,5 +334,28 @@ food_6.interests << food_interest
 food_7.interests << food_interest
 food_8.interests << food_interest
 food_9.interests << food_interest
+food_10.interests << food_interest
 food_11.interests << food_interest
 
+# Populate the database with history-based activities
+history_1 = Activity.create({ title: "US History Overview 1: Jamestown to the Civil War", content_type: "video", completion_time: 19, link: "https://www.youtube.com/watch?v=ghgPq2wjQUQ", content: "" })
+history_2 = Activity.create({ title: "Appomattox Court House and Lincoln's Assassination", content_type: "video", completion_time: 6, link: "https://www.youtube.com/watch?v=Frpq_A7Ky_Q", content: "" })
+history_3 = Activity.create({ title: "US History Overview 2 - Reconstruction to the Great Depression", content_type: "video", completion_time: 15, link: "https://www.youtube.com/watch?v=zmBV87XA52Q", content: "" })
+history_4 = Activity.create({ title: "US History Overview 3 - WWII to Vietnam", content_type: "video", completion_time: 15, link: "https://www.youtube.com/watch?v=j2eKaxU-8kA", content: "" })
+history_5 = Activity.create({ title: "Communism", content_type: "video", completion_time: 16, link: "https://www.youtube.com/watch?v=MmRgMAZyYN0", content: "" })
+history_6 = Activity.create({ title: "Korean War Overview", content_type: "video", completion_time: 17, link: "https://www.youtube.com/watch?v=MEGyRgYJKEY", content: "" })
+history_7 = Activity.create({ title: "Bay of Pigs Invasion", content_type: "video", completion_time: 14, link: "https://www.youtube.com/watch?v=XmkbAduMD_E", content: "" })
+history_8 = Activity.create({ title: "Cuban Missile Crisis", content_type: "video", completion_time: 19, link: "https://www.youtube.com/watch?v=VO40SpSBjbc", content: "" })
+history_9 = Activity.create({ title: "Vietnam War", content_type: "video", completion_time: 18, link: "https://www.youtube.com/watch?v=9e9GWdT2pEQ", content: "" })
+history_10 = Activity.create({ title: "Pattern of US Cold War Interventions", content_type: "video", completion_time: 10, link: "https://www.youtube.com/watch?v=aNkrawaaiZQ", content: "" })
+
+history_1.interests << history_interest
+history_2.interests << history_interest
+history_3.interests << history_interest
+history_4.interests << history_interest
+history_5.interests << history_interest
+history_6.interests << history_interest
+history_7.interests << history_interest
+history_8.interests << history_interest
+history_9.interests << history_interest
+history_10.interests << history_interest

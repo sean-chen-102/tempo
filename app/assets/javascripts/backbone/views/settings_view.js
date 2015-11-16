210fc5e1693cdc5551a848d716c7ed10bb2b3cdc
jeremypress <jeremypress@berkeley.edu>
2015-11-15 21:18:29 -0800

    moved settings to template system, cleaned up interests logic

--- app/assets/javascripts/backbone/views/settings_view.js
+++ app/assets/javascripts/backbone/views/settings_view.js
@@ -6,20 +6,32 @@ var SettingsView = Backbone.View.extend({
 	events:{
     	"click #submit-interests":"saveInterests",
 	},
+	renderData : function(data){
+		var that = this;
+		console.log(data);
+		//Iterate throught he collections of Activities and create a template
+		var numInterests = 0;
+		data.each(function(model){
+			var checked = "";
+			if (that.userInterests.indexOf(model.get('name')) > -1) { 
+				checked = "checked";
+			}
+			that.templateData.push({"name": model.get("name"), "checked" : checked, "index": numInterests})
+			numInterests += 1
+		});
+		that.numInterests = numInterests;
+	   	var template = JST["backbone/templates/activities/settings"]({
+			data: that.templateData,
+    	});
+        this.$el.html(template);
+    },
 	initialize: function(options){
 		console.log("settings initialize called");
 		this.userInterests = [];
+		this.templateData = [];
 		this.user = null;
 		this.options = options;
 		this.numInterests = 0;
-		// Backbone.Events.on("user-interests", function(interests) {
-		// 	console.log("user-interests");
-		// 	console.log(interests[1]);
-  //  			this.userInterests = interests[0];
-  //  			this.id = interests[1];
-  //  			console.log(this.id);
-
-		// 	});
 	},
 	saveInterests : function(options) {
 		var that = this;
@@ -43,16 +55,14 @@ var SettingsView = Backbone.View.extend({
       			console.log("failed to save interest");
       		}
     	});
-
-
-	},
-	getUserInterests : function(callback, args){
+    },
+	getUserInterests : function(){
 		// Fetches the user interests from the database
-		var that = this;
 		var interests = new Interests();
 		interests.url = "/api/users/" + this.user.id + "/interests";
 		var token = Cookies.get('login-token');
 		interests.url += "?token=" + token;
+		var that = this;
 		interests.fetch({
 			success: function(data){
 				that.userInterests = [];
@@ -60,54 +70,27 @@ var SettingsView = Backbone.View.extend({
 					console.log("hi")
 					that.userInterests.push(model.get('name'));
 				});
-				if (args){
-					callback(args);
-				} else {
-					callback();
-				}
+				that.getInterests();
 			}
 		});
 	},
-	getInterests : function(that){
+	getInterests : function(){
+		console.log("pls");
 		var interests = new Interests();
+		var that = this;
 		interests.fetch({
 			success: function(data){
-				that.renderData(data, that);
+				that.renderData(data);
 			}
 		});
 	},
 	render : function (options){
+		this.templateData = [];
+		console.log("render");
 		this.user = options["user"];
+		this.getUserInterests();
 		// Set scope, construct new activity collection, call fetch, render data on callback function
 		var that = this; // To fix callback scoping error
-		var template = JST["backbone/templates/activities/settings"]({
-    	    });
-        this.$el.html(template);
-		this.getUserInterests(this.getInterests, this);
+		console.log(that.templateData);
 	}, 
-	renderData : function(data, that){	    	
-		//TODO: Create and import handlebars for templating			
-		var html = "<h4 style='color: #1abc9c;'> Interest List </h4> <br>"
-					+ "<table> <thead> <tr> <th> </th>"
-					+ "<th colspan='3'></th> </tr> </thead>" 
-					+ " <tbody> ";
-		//Iterate throught he collections of Activities and create a template
-		var numInterests = 0;
-		console.log(that.userInterests);
-		data.each(function(model){
-			var checked = "";
-			if (that.userInterests.indexOf(model.get('name')) > -1) { 
-				checked = "checked";
-			}
-			html += "<tr>" 
-					+   " <td> <input type=checkbox name=" + model.get('name') +  " id=interest-" + numInterests + " " + checked + " >  </td>"
-					+ "<td> " +  model.get('name') + " </td>"
-				+ "</tr>";	
-			numInterests += 1
-		});
-		that.numInterests = numInterests;
-		html += " </tbody> </table> </br> ";
-		html += "<button id=submit-interests> save </button> <br>"; 
-		$(that.el).append(html);
-    }
 });

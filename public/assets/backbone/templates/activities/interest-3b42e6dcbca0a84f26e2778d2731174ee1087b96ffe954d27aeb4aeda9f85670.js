65fe6e7c81ed356f7875d27f999f4fcaeea659da
Dalton Boll <daltonboll@gmail.com>
2015-11-23 03:29:26 -0800

    Precompile assets for Heroku

--- public/assets/backbone/templates/activities/interest-3b42e6dcbca0a84f26e2778d2731174ee1087b96ffe954d27aeb4aeda9f85670.js
+++
@@ -1,2 +0,0 @@
-(function() { this.JST || (this.JST = {}); this.JST["backbone/templates/activities/interest"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<a href=\'tempo#home\'> Go Back</a>\n');}return __p.join('');};
-}).call(this);

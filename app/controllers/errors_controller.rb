925f2209635e8586aca1d652127b984b5c4c6b04
Dalton Boll <daltonboll@gmail.com>
2015-11-24 01:06:49 -0800

    Prevent Rails 404 errors from showing

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,12 @@
+class ErrorsController < ApplicationController
+
+  # For handling 404 errors
+  def not_found
+    respond_to do |format|
+      format.html { render status: 404 }
+    end
+  rescue ActionController::UnknownFormat
+    render status: 404, text: "nope"
+  end
+
+end
